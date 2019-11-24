10ff9d0cfeb5157456c08c81c2d4b19272c1540e
David Vega <dvega920@gmail.com>
2019-11-24 14:40:35 -0500

    initial files added

---
+++ index.js
@@ -0,0 +1,54 @@
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require("fs");
+const util = require("util");
+const pdf = require("html-pdf");
+const open = require("open");
+const generateHTML = require("./generateHTML.js");
+
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "username",
+            message: "What is your GitHub username?"
+        },
+        {
+            type: "list",
+            name: "colors",
+            choices: ["green", "blue", "pink", "red"]
+        }
+    ]);
+}
+promptUser()
+    .then(function (userAns) {
+        data = userAns;
+        console.log(userAns);
+        let api = `https://api.github.com/users/${data.username}`;
+        return axios.get(api)
+    })
+    .then(function (response) {
+        console.log(response);
+        const html = generateHTML(data, response);
+        return writeFileAsync("index.html", html);
+    })
+    .then(function () {
+        console.log("Successfully wrote to index.html");
+        let convert = fs.readFileSync("./index.html", "utf-8");
+        let options = { format: Letter };
+    })
+    .then(function () {
+        pdf.create(convert, options).toFile("index.pdf", (err, res) => {
+            open("index.pdf");
+
+            if (err)
+                return reject(err)
+            return resolve(res)
+        })
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
